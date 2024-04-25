7864a29c62d4b5a309e1169b1ecf93c3abbced43
vararais <varadigmaisnann@gmail.com>
2024-04-25 17:20:57 +0700

    Menambah If Else

--- LinkList/LinkList.cpp
+++ LinkList/LinkList.cpp
@@ -53,7 +53,24 @@ void addNode() {
 bool serachNode(int nim, Node* current, Node* previous) {
     previous = START;
     current = START;
-    
+    while (current != NULL && nim > current->noMhs)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    if (current == NULL)
+    {
+        return false;
+    }
+    else if (current->noMhs == nim)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 int main()
 {
