4b541d653f0c05542c6a227761ce94c835ddf636
vararais <varadigmaisnann@gmail.com>
2024-04-25 17:05:05 +0700

    Menambah Elemen Node

--- LinkList/LinkList.cpp
+++ LinkList/LinkList.cpp
@@ -10,7 +10,15 @@ struct Node {
 Node* START = NULL;
 
 void addNode() {
-
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukan NIM: ";
+    cin >> nim;
+    cout << "Masukan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
 }
 int main()
 {
