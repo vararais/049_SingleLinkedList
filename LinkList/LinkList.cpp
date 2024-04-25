2be76b04df7cda2b1333b8d67f16ff1cdb8416ed
vararais <varadigmaisnann@gmail.com>
2024-04-25 17:14:39 +0700

    Menambah While

--- LinkList/LinkList.cpp
+++ LinkList/LinkList.cpp
@@ -34,6 +34,17 @@ void addNode() {
 
     Node* previous = START;
     Node* current = START;
+
+    while ((current != NULL) && (nim >= current->noMhs))
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
 }
 int main()
 {
