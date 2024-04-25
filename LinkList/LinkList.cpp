db083efae6a3cfd9548db6c1dbb4b5f939161e0e
vararais <varadigmaisnann@gmail.com>
2024-04-25 17:07:47 +0700

    Melengkapi Elemen Elemen

--- LinkList/LinkList.cpp
+++ LinkList/LinkList.cpp
@@ -19,6 +19,14 @@ void addNode() {
     cin >> nama;
     nodeBaru->noMhs = nim;
     nodeBaru->name = nama;
+
+    if (START == NULL || nim <= START->noMhs) {
+        if (START != NULL && nim == START->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+    }
 }
 int main()
 {
